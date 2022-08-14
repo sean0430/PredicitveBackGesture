e1e7b31846f751d8cc9f16c6bc2a3a1e4e2c0982
Sean <sean0430@gmail.com>
2022-08-14 22:39:51 +0800

    Second Activity

--- app/src/main/java/com/example/predicitvebackgesture/SecondActivity.kt
+++ app/src/main/java/com/example/predicitvebackgesture/SecondActivity.kt
@@ -1,21 +1,17 @@
 package com.example.predicitvebackgesture
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.predicitvebackgesture.ui.theme.PredicitveBackGestureTheme
 
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,15 +21,11 @@ class MainActivity : ComponentActivity() {
                     contentAlignment = Alignment.Center,
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color.Yellow)
-                        .clickable {
-                            startActivity(Intent(this, SecondActivity::class.java))
-                        }
+                        .background(Color.Green)
                 ) {
-                    Greeting("First")
+                    Greeting("Second")
                 }
             }
         }
     }
 }
-
