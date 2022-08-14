b9e07eb2cb0e027d97760156d12c32ad2d17e62c
Sean <sean0430@gmail.com>
2022-08-14 23:15:20 +0800

    press back twice to exit

--- app/src/main/java/com/example/predicitvebackgesture/MainActivity.kt
+++ app/src/main/java/com/example/predicitvebackgesture/MainActivity.kt
@@ -2,20 +2,52 @@ package com.example.predicitvebackgesture
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.predicitvebackgesture.ui.theme.PredicitveBackGestureTheme
+import java.util.*
+import kotlin.system.exitProcess
+
 
 class MainActivity : ComponentActivity() {
+    private var isExit = false
+    private var hasTask = false
+
+    var timerExit: Timer = Timer()
+    var task: TimerTask = object : TimerTask() {
+        override fun run() {
+            isExit = false
+            hasTask = true
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (!isExit) {
+                isExit = true
+                Toast.makeText(
+                    this, "Press again to exit", Toast.LENGTH_SHORT).show()
+                if (!hasTask) {
+                    timerExit.schedule(task, 2000)
+                }
+            } else {
+                finish()
+                exitProcess(0)
+            }
+        }
+        return false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
